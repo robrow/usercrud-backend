a13dead2043aa3e8c2c6cda47796aa79a1f1edcd
Robin Rowinski <rowinski.robin@gmail.com>
2024-04-28 14:43:52 +0200

    Add CORS rule

--- src/main/java/net/rowinski/demo/crud/CrudApplication.java
+++ src/main/java/net/rowinski/demo/crud/CrudApplication.java
@@ -2,12 +2,28 @@ package net.rowinski.demo.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+			}
+		};
 	}
 
 }
